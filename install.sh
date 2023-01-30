64b6e706b604d5820c2b20e40c3a924261b64cf7
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-01-30 13:27:10 +0100

    app tienda

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 
+echo "Tareas (por etapas) del pipleline JenkinsFile..."
+
 echo "Instalando la tienda App PC Componentes"
+echo "Descargar repositorio de AppTienda..."
+echo "Actualizar versiones de los dockerfiles en Docker Hub..."
+echo "Realizar el build del proyecto ..."
+echo "Fases de testeo..."
+echo "Matar los contenedores en ejecución de la verión anterior..."
+echo "Levantar los nuevos contenedores actualizados ..."
