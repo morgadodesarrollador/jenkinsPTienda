836a0c35f8aba405402d2d93d11e0d49bac6bb41
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-02-09 12:21:45 +0100

    levantar nginx

--- build/install.sh
+++ build/install.sh
@@ -1,6 +1,8 @@
 
 echo "Desplegando la app rde gdfaspTienda"
 
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
+docker-compose -f ./build/docker-compose.yml down
+
+docker-compose -f ./build/docker-compose.yml build
+docker-compose -f ./build/docker-compose.yml up -d
 
