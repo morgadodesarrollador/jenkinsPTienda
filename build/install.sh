b7980dc4c84d1400d3f808adaf1262ddf25c342e
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-02-09 12:46:11 +0100

    levantar nginx

--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,8 @@
 
 echo "Desplegando la app rde sgdsfgsdfggdfaspTienda"
 
-docker-compose -f ./build/docker-compose.yml down 
 
 docker-compose -f ./build/docker-compose.yml build --no-cache
+docker-compose -f ./build/docker-compose.yml down 
 docker-compose -f ./build/docker-compose.yml up -d --remove-orphans
 
