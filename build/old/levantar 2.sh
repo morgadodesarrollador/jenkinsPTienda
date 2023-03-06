08b367d35d351ecaf4eafea1272918f9076f886f
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-03-06 14:25:48 +0100

    dockerfile nginx

--- build/old/levantar 2.sh
+++
@@ -1,3 +0,0 @@
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
-# docker exec -it apiNest /bin/bash
--- build/parar.sh
+++
@@ -1,6 +0,0 @@
-docker-compose -f docker-compose.yml down
-docker rmi apinestincidencias:latest 
-docker image prune
-
-sudo rm -rf postgres
-sudo rm -rf web
